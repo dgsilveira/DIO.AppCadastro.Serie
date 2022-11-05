6bfed1a9013059f241fdff4db912cd2cc8b101a5
dgsilveira <dgsilveira@sga.pucminas.br>
2022-11-05 18:27:46 -0300

    feat: Model Serie

---
+++ DIO.AppCadastro.Serie/Classes/Serie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DIO.AppCadastro.Serie
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            return "" + "Genero: " + this.Genero + Environment.NewLine +
+                "Título: " + this.Titulo + Environment.NewLine +
+                "Descrição: " + this.Descricao + Environment.NewLine +
+                "Ano de Início: " + this.Ano + Environment.NewLine +
+                "Excluído: " + this.Excluido;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
